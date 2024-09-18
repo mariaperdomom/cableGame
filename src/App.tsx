930e2997a577386625a860c3f283083caeef93f3
Maria Alejandra Perdomo <alejandrav3115@gmail.com>
2024-09-18 15:47:12 -0500

    Ajustes de componentes en los videos de inicio y fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React, { useState } from 'react';
-import { AspectRatio, Container } from '@mantine/core';
+import { Container } from '@mantine/core';
 import Game from './components/Game';
 import Register from './components/Register';
-import ReactPlayer from 'react-player';
+import Welcome from './components/Welcome';
+import End from './components/End';
 
 const App: React.FC = () => {
   const [ actions, setActions ] = useState<string>('');
   const [ userCode, setUserCode ] = useState<string>('');
   const [ userName, setUserName ] = useState<string>('');
-
-   // Función que se llama cuando el video termina
-   const handleVideoEnd = () => {
-    setTimeout(() => {
-      setActions('');
-    }, 500); 
-  };
     
   return (
     <Container 
@@ -30,20 +24,7 @@ const App: React.FC = () => {
       }}
     >
       { actions === '' && 
-        <AspectRatio 
-          ratio={9 / 16} 
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', cursor: 'pointer' }}
-          onClick={()=> setActions('register')}
-        >
-          <ReactPlayer 
-            url={'../assets/Intro.mp4'}
-            playing={true}
-            loop={true}
-            height={'100%'}
-            width={'100%'}
-            style={{ objectFit: 'cover' }}
-          />
-        </AspectRatio>
+        <Welcome setActions={setActions} />
       }
       { actions === 'register' && 
         <Register setActions={setActions} setUserCode={setUserCode} userName={userName} setUserName={setUserName}/>
@@ -52,19 +33,7 @@ const App: React.FC = () => {
           <Game setActions={setActions} userCode={userCode} userName={userName}/>
       }
       { actions === 'end' &&
-        <AspectRatio 
-          ratio={9 / 16} 
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-        >
-          <ReactPlayer 
-            url={'../assets/Cierre.mp4'}
-            playing={true}
-            onEnded={handleVideoEnd}
-            height={'100%'}
-            width={'100%'}
-            style={{ objectFit: 'cover' }}
-          />
-        </AspectRatio>
+        <End setActions={setActions} />
       }
     </Container>
   );
---
+++ src/components/End.tsx
@@ -0,0 +1,35 @@
+import { AspectRatio } from '@mantine/core'
+import ReactPlayer from 'react-player';
+
+interface Props {
+    setActions: (action: string) => void; 
+}
+
+const End = (props: Props) => {
+    const { setActions } = props;
+
+     // Función que se llama cuando el video termina
+    const handleVideoEnd = () => {
+        setTimeout(() => {
+        setActions('');
+        }, 500); 
+    };
+
+    return (
+        <AspectRatio 
+          ratio={9 / 16} 
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+        >
+            <ReactPlayer 
+                url={'../assets/Cierre.mp4'}
+                playing={true}
+                onEnded={handleVideoEnd}
+                height={'100%'}
+                width={'100%'}
+                style={{ objectFit: 'cover' }}
+            />
+        </AspectRatio>
+    )
+}
+
+export default End;
